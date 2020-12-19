1ef14d34d3f7d152d35d011436ee0b55e7233769
Margot <tjibzo@github.com>
2020-12-19 17:09:49 +0100

    fin du camp, il reste quelques trucs à faire

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-$("#top").text("Hello world");
-
 var score = 0;
 var music = document.getElementById("music");
 var is_running = false;
-
-$("body").keypress(GameLogic());
-$("#play").click(Main());
-$("#score").text("Score : " + document.getElementById("score"));
-
 var tiles = InitTab();
-var newTab = [];
 
-function Main(id, tiles) {
-    var score = 0;
-    var is_running = true;
-    for (var i = 0; i < tiles.length; i++) {
-        window.setTimeout(InitLine, 400 * index, id, tiles[i]);
+$("body").keypress(GameLogic);
+$("#play").click(Main);
+$("#score").text("Score : " + score);
+
+
+function Main() {
+    if (!is_running) {
+        var score = 0;
+        $("#score").text("Score : " + score);
+        is_running = true;
+        music.play();
+        for (var i = 0; i < tiles.length; i++) {
+            window.setTimeout(LaunchGame, 400 * i, i, tiles);
+        }
     }
 }
 
 function InitTab() {
     var newTab = [];
-    for (var j = 0; j < 152; j++) {
+    for (var j = 0; j < 151; j++) {
         newTab.push([Rng(2), Rng(2), Rng(2), Rng(2)]);
     }
-    for (var k = 0; j < 10; k++) {
+    for (var k = 0; k != 9; k++) {
         newTab.push([0, 0, 0, 0]);
     }
     newTab.push([2, 2, 2, 2]);
-    return (newTab)
+    return (newTab);
 }
 
 function Rng(max) {
@@ -41,23 +42,76 @@ function LaunchGame(id, tiles) {
     }
 }
 
-function InitLine(id, tiles) {
+function InitLine(id, tile) {
     for (var index = 0; index < tile.lengh; index++) {
         if (tile[index] === 1) {
-            if (isRunning === true) {
-                //appel
+            if (is_running === true) {
+                ChangeColor("#line-" + id + "-" + (index + 1), (index + 1));
             }
         } else if (tile[index] === 2) {
-            isRunning = false;
+            is_running = false;
             $("#result").text("Good Job !");
         } else {
-            //appel
+            ClearColor("#line-" + id + "-" + (index + 1));
         }
     }
 }
 
-function ChangeColor() {
+function ChangeColor(id, nb) {
     if (nb === 1) {
         $(id).css("background-color", "rgb(104, 4, 4)");
+    } else if (nb === 2) {
+        $(id).css("background-color", "rgb(254, 19, 16");
+    } else if (nb === 3) {
+        $(id).css("background-color", "rgb(224,223,229)");
+    } else if (nb === 4) {
+        $(id).css("background-color", "rgb(9, 47, 0)");
     }
+}
+
+function ClearColor(id) {
+    $(id).css("background-color", "transparent");
+}
+
+function GameLogic(event) {
+    if (is_running == true) {
+        if (event.keyCode == 97) {
+            if ($("#line-9-1").css("background-color") === "rgb(0, 128, 0)") {
+                score++;
+            } else {
+                score--;
+            }
+            $("#line-9-1").css("background-color", "#000");
+        }
+        if (event.keyCode == 122) {
+            if ($("#line-9-2").css("background-color") === "rgb(254, 19, 16)") {
+                score++;
+            } else {
+                score--;
+            }
+            $("#line-9-2").css("background-color", "#000");
+        }
+        if (event.keyCode == 101) {
+            if ($("#line-9-3").css("background-color") === "rgb(224, 223, 229)") {
+                score++;
+            } else {
+                score--;
+            }
+            $("#line-9-3").css("background-color", "#000");
+        }
+        if (event.keyCode == 114) {
+            if ($("#line-9-4").css("background-color") === "rgb(9, 47, 0)") {
+                score++;
+            } else {
+                score--;
+            }
+            $("#line-9-4").css("background-color", "#000");
+        }
+        $("#score").text("Score : " + score);
+        window.setTimeout(ClearColor, 100, "#line-9-1");
+        window.setTimeout(ClearColor, 100, "#line-9-2");
+        window.setTimeout(ClearColor, 100, "#line-9-3");
+        window.setTimeout(ClearColor, 100, "#line-9-4");
+    }
+
 }
