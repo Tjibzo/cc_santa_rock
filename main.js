85a4afa73767e973259b369d3b880f0376199863
Margot <tjibzo@github.com>
2020-12-19 14:24:54 +0100

    js

--- main.js
+++ main.js
@@ -11,21 +11,53 @@ $("#score").text("Score : " + document.getElementById("score"));
 var tiles = InitTab();
 var newTab = [];
 
-function InitTab() {
-    var newTab = [];
-    for (var j = 0; j < 152; j++) {
-        var newTab = newTab + [Rng(2), Rng(2), Rng(2), Rng(2)];
-    }
-    for (var k = 0; j < 10; k++) {
-        var newTab = newTab + [0, 0, 0, 0]
-    }
-    var newTab = newTab + [2, 2, 2, 2]
-}
-
-function Main() {
+function Main(id, tiles) {
     var score = 0;
     var is_running = true;
     for (var i = 0; i < tiles.length; i++) {
-        window.setTimeout()
+        window.setTimeout(InitLine, 400 * index, id, tiles[i]);
+    }
+}
+
+function InitTab() {
+    var newTab = [];
+    for (var j = 0; j < 152; j++) {
+        newTab.push([Rng(2), Rng(2), Rng(2), Rng(2)]);
+    }
+    for (var k = 0; j < 10; k++) {
+        newTab.push([0, 0, 0, 0]);
+    }
+    newTab.push([2, 2, 2, 2]);
+    return (newTab)
+}
+
+function Rng(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function LaunchGame(id, tiles) {
+    for (var index = 0; index < tiles.length; index++) {
+        window.setTimeout(InitLine, 400 * index, id, tiles[index]);
+    }
+}
+
+function InitLine(id, tiles) {
+    for (var index = 0; index < tile.lengh; index++) {
+        if (tile[index] === 1) {
+            if (isRunning === true) {
+                //appel
+            }
+        } else if (tile[index] === 2) {
+            isRunning = false;
+            $("#result").text("Good Job !");
+        } else {
+            //appel
+        }
+    }
+}
+
+function ChangeColor() {
+    if (nb === 1) {
+        $(id).css("background-color", "rgb(104, 4, 4)");
     }
 }
